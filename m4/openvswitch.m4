igsilya/ovs

commit 930f135f5ddc4372c1615bc7674dda35c229b6bd

configure: explicitly disable avx512 if bintuils check fails
This commit explicitly disables avx512f if the binutils assembler
check fails to correctly assemble its input.

Without this fix, there is a possibility that users can see undefined
behaviour when compiling with -march=native on a CPU which supports
avx512 and with a buggy binutils version (v2.30 and 2.31), without a
backported fix, if the compiler's vectorizing optimizations convert
scalar code to avx512 instructions.

Signed-off-by: Harry van Haaren <harry.van.haaren@intel.com>
Signed-off-by: Ian Stokes <ian.stokes@intel.com>

m4/openvswitch.m4
@@ -426,8 +426,12 @@ AC_DEFUN([OVS_CHECK_BINUTILS_AVX512],
          CFLAGS="$CFLAGS -DHAVE_LD_AVX512_GOOD"
        else
          ovs_cv_binutils_avx512_good=no
+         dnl Explicitly disallow avx512f to stop compiler auto-vectorizing
+         dnl and causing zmm usage with buggy binutils versions.
+         CFLAGS="$CFLAGS -mno-avx512f"
        fi
      else
+       dnl non x86_64 architectures don't have avx512, so not affected
        ovs_cv_binutils_avx512_good=no
      fi])
      rm $OBJFILE

commit fd8b34470919a237647b1c4315329cfd22b951d0

m4: Don't add -mno-avx512f if compiler doesn't support it.
When building using an older compiler that doesn't support avx512,
we are adding '-mno-avx512f' into the command line, which results
with a build failure:

  gcc: error: unrecognized command line option '-mno-avx512f'

This is a case, for example, while trying to build OVS with GCC 4.8.

Fix that by avoiding binutils check when compiler doesn't understand
-mavx512f.

Later in the call chain there is also an explicit check for -mavx512f
support, but it will just use the cached result, so it's not a problem.

Fixes: 930f135f5ddc ("configure: explicitly disable avx512 if bintuils check fails")
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

m4/openvswitch.m4
@@ -475,7 +475,9 @@ dnl The checking of binutils funcationality instead of LD version is similar
 dnl to as how DPDK proposes to solve this issue:
 dnl   http://patches.dpdk.org/patch/71723/
 AC_DEFUN([OVS_CHECK_BINUTILS_AVX512],
-  [AC_CACHE_CHECK(
+  [OVS_CHECK_CC_OPTION(
+   [-mavx512f],
+   [AC_CACHE_CHECK(
     [binutils avx512 assembler checks passing],
     [ovs_cv_binutils_avx512_good],
     [dnl Assemble a short snippet to test for issue in "build-aux" dir:
@@ -496,7 +498,8 @@ AC_DEFUN([OVS_CHECK_BINUTILS_AVX512],
      else
        dnl non x86_64 architectures don't have avx512, so not affected
        ovs_cv_binutils_avx512_good=no
-     fi])
+     fi])],
+    [ovs_cv_binutils_avx512_good=no])
    if test "$ovs_cv_binutils_avx512_good" = yes; then
      AC_DEFINE([HAVE_LD_AVX512_GOOD], [1],
                [Define to 1 if binutils correctly supports AVX512.])